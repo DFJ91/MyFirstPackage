511ff88eab4b563fddadef7a01f0520de399c88f
DFJ91 <davidfernandez.j4@gmail.com>
2017-10-29 13:51:40 +0100

    Add files via upload

---
+++ R/DummyFunctions.R
@@ -0,0 +1,19 @@
+## Funciones Dummy ##
+
+RealMadrid_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("Me gusta el Real Madrid!")
+  }
+  else {
+    print("Prefiero otros equipos")
+  }
+}
+
+BMW_M_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("Soy fan de BMW M series")
+  }
+  else {
+    print("Prefiero Mercedes AMG style")
+  }
+}
