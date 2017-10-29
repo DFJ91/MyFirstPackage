fbde2ac26be0bcca02c3325c16cf3702c8ed59c3
DFJ91 <davidfernandez.j4@gmail.com>
2017-10-29 14:04:30 +0100

    Add files via upload

---
+++ R/MyFunctions.R
@@ -0,0 +1,18 @@
+
+RealMadrid_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("Me gusta el Real Madrid!")
+  }
+  else {
+    print("Prefiero otros equipos")
+  }
+}
+
+BMW_M_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("Soy fan de BMW M series")
+  }
+  else {
+    print("Prefiero Mercedes AMG style")
+  }
+}
